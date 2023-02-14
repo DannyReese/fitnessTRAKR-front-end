8ddeb1bcc504ead2e9449ba61e59e4d843d8cac3
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-02-14 18:31:38 -0500

    logged out actvity page done with very basic css started just to add separation

--- src/routes/ActivityPage.js
+++ src/routes/ActivityPage.js
@@ -1,13 +1,41 @@
 import {NavBar} from "../components/NavBar";
+import {showActivites} from '../Api.fetch'
+import { useState,useEffect } from "react";
+import ActivityPageCss from '../css/ActivtiyPage.module.css'
 
 export const ActivityPage = ()=>{
-    return(
+    const [activities, setActivities] = useState([])
+
+    const getActivities = async () => {
+        // add condtion in here if user is logged in return all routines 
+        const res = await showActivites()
+        setActivities(res)
+    }
+
+    useEffect(() => { getActivities() }, []);
+
+    return (
         <div>
-        <NavBar />
-        <div>
-            ActivityPage
+            <NavBar />
+            <div className={ActivityPageCss.container}>{
+                activities ? activities.map(a => {
+                    return (
+                        <div className={ActivityPageCss.activity} key={a.id}>
+                            <div>
+                                <h2>{a.name}</h2>
+                            </div>
+                            <div>
+                                {a.description}
+                            </div>
+                        </div>
+                    )
+                }) : null
+            }
+
+                ActivityPage
+            </div>
         </div>
-    </div>)
-};
+    )
+}
 
 export default ActivityPage
--- src/routes/RoutinePage.js
+++ src/routes/RoutinePage.js
@@ -1,6 +1,6 @@
 import { NavBar } from "../components/NavBar"
 import { useState, useEffect } from "react"
-import  {pubRoutines}  from '../Api.fetch'
+import { pubRoutines } from '../Api.fetch'
 import RoutinePageCss from '../css/RoutinePage.module.css'
 
 const RoutinePage = () => {
@@ -8,34 +8,35 @@ const RoutinePage = () => {
     const [pubRoutine, setPubRoutines] = useState([])
 
     const getPubRoutines = async () => {
+        // add condtion in here if user is logged in return all routines 
         const res = await pubRoutines()
         setPubRoutines(res)
     }
 
-    useEffect(()=>{getPubRoutines()},[]);
+    useEffect(() => { getPubRoutines() }, []);
 
     return (
         <div>
             <NavBar />
-            <div>{
-                pubRoutine ? pubRoutine.map(pr=>{
-                    return(
+            <div className={RoutinePageCss.container}>{
+                pubRoutine ? pubRoutine.map(pr => {
+                    return (
                         <div className={RoutinePageCss.routine} key={pr.id}>
-                        <div><h2>{pr.name}</h2></div>
-                        <div>{pr.goal}</div>
-                        <div><h3>Activities</h3>{pr.activities.map(a=>{
-                            return (
-                                <div className={RoutinePageCss.activities}key={a.id}>
-                                <div>{a.name}</div>
-                                <div>{a.description}</div>
-                                <div>Duration {a.duration}</div>
-                                <div>Dount {a.count}</div>
-                               </div>
-                            )
-                        })}</div>
+                            <div><h2>{pr.name}</h2></div>
+                            <div>{pr.goal}</div>
+                            <div><h3>Activities</h3>{pr.activities.map(a => {
+                                return (
+                                    <div className={RoutinePageCss.activities} key={a.id}>
+                                        <div>{a.name}</div>
+                                        <div>{a.description}</div>
+                                        <div>Duration {a.duration}</div>
+                                        <div>Dount {a.count}</div>
+                                    </div>
+                                )
+                            })}</div>
                         </div>
                     )
-                }):null
+                }) : null
             }
 
                 routine page
